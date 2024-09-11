2c6975d6db953b1556ccbf2ba2fe00190eb69e26
murilott <murilo_teichert@hotmail.com>
2024-09-11 14:11:24 -0300

    adicionado alguns métodos

--- src/main/java/br/edu/univille/poo/jpa/service/TarefaService.java
+++ src/main/java/br/edu/univille/poo/jpa/service/TarefaService.java
@@ -7,6 +7,9 @@ import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -37,6 +40,54 @@ public class TarefaService {
         return todos;
     }
 
+    public List<Tarefa> obterTodosFinalizado() {
+        List<Tarefa> todos = new ArrayList<Tarefa>();
+
+        for( var tarefa : tarefaRepository.findAll()) {
+            if (tarefa.isFinalizado()) {
+                todos.add(tarefa);
+            }
+        }
+
+        return todos;
+    }
+
+    public List<Tarefa> obterTodosAtrasados() {
+        List<Tarefa> todos = new ArrayList<Tarefa>();
+
+        // LocalDateTime ldt = LocalDateTime.now();
+        LocalDate localDate = LocalDate.now();
+        ZoneId z = ZoneId.of( "Brazil/East" );
+        Date today = Date.from(localDate.atStartOfDay(z).toInstant());
+        // LocalDate today = LocalDate.now( z );  // Always pass a time zone.
+
+        for( var tarefa : tarefaRepository.findAll()) {
+            if (tarefa.getDataPrevista().after(today)) {
+                todos.add(tarefa);
+            }
+        }
+
+        return todos;
+    }
+
+    public List<Tarefa> obterTodosNaoFinalizadoEntreData(Date inicio, Date fim) {
+        List<Tarefa> todos = new ArrayList<Tarefa>();
+
+        // LocalDateTime ldt = LocalDateTime.now();
+        LocalDate localDate = LocalDate.now();
+        ZoneId z = ZoneId.of( "Brazil/East" );
+        Date today = Date.from(localDate.atStartOfDay(z).toInstant());
+        // LocalDate today = LocalDate.now( z );  // Always pass a time zone.
+
+        for( var tarefa : tarefaRepository.findAll()) {
+            if (tarefa.getDataPrevista().after(inicio) && tarefa.getDataPrevista().before(fim)) {
+                todos.add(tarefa);
+            }
+        }
+
+        return todos;
+    }
+
     public Tarefa incluir(Tarefa tarefa) {
         tarefa.setId(0);
         if(Strings.isBlank(tarefa.getTitulo())){
@@ -45,38 +96,49 @@ public class TarefaService {
         if(Strings.isBlank(tarefa.getDesc())){
             throw new RuntimeException("Descrição não informado.");
         }
-        if(tarefa.isFinalizado()){
+        if(tarefa.getDataPrevista() == null){
+            throw new RuntimeException("Data prevista não informada.");
+        }
+
+        if(tarefa.isFinalizado()){ // tirar
             throw new RuntimeException("Tarefa não pode estar finalizada.");
         }
         // Adicionar verificação de datas posteriormente
+        
         tarefa = tarefaRepository.save(tarefa);
         return tarefa;
     }
 
     public Tarefa atualizar(Tarefa tarefa) {
         Tarefa antigo = tarefaRepository.findById(tarefa.getId()).orElse(null);
+        
         if(antigo == null){
             throw new RuntimeException("tarefa não foi encontrada.");
         }
-        antigo.setNome(tarefa.getNome());
-        antigo.setCpf(tarefa.getCpf());
-        antigo.setEmail(tarefa.getEmail());
-        if(Strings.isBlank(tarefa.getNome())){
-            throw new RuntimeException("Nome não informado.");
+        if (tarefa.isFinalizado()) {
+            throw new RuntimeException("Tarefa finalizada não pode ser atualizada.");
         }
-        if(Strings.isBlank(tarefa.getCpf())){
-            throw new RuntimeException("CPF não informado.");
+
+        antigo.setTitulo(tarefa.getTitulo());
+        antigo.setDesc(tarefa.getDesc());
+        // antigo.setFinalizado(tarefa.isFinalizado());
+        antigo.setDataPrevista(tarefa.getDataPrevista());
+
+        if(Strings.isBlank(tarefa.getTitulo())){
+            throw new RuntimeException("Título não informado.");
         }
-        for(var p : tarefaRepository.findAllByCpf(tarefa.getCpf())){
-            if(antigo.getId() != p.getId()){
-                throw new RuntimeException("CPF já está cadastrado.");
-            }
+        if(Strings.isBlank(tarefa.getDesc())){
+            throw new RuntimeException("Descrição não informado.");
         }
-        for(var p : tarefaRepository.findAllByNomeContaining(tarefa.getNome())){
-            if(antigo.getId() != p.getId()){
-                throw new RuntimeException("Nome já está cadastrado.");
-            }
+        if(tarefa.getDataPrevista() == null){
+            throw new RuntimeException("Data prevista não informada.");
         }
+        // for(var p : tarefaRepository.findAllByNomeContaining(tarefa.getNome())){
+        //     if(antigo.getId() != p.getId()){
+        //         throw new RuntimeException("Nome já está cadastrado.");
+        //     }
+        // }
+        
         return tarefaRepository.save(antigo);
     }
 
@@ -86,5 +148,15 @@ public class TarefaService {
             throw new RuntimeException("tarefa não encontrada.");
         }
         tarefaRepository.delete(antigo);
+    }
+
+    public void finalizar(Tarefa tarefa) {
+        var antigo = tarefaRepository.findById(tarefa.getId()).orElse(null);
+        if(antigo == null){
+            throw new RuntimeException("tarefa não encontrada.");
+        }
+        antigo.setFinalizado(true);
+
+        tarefaRepository.save(antigo);
     }
 }
